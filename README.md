50fc2b4df010e0e145124e69c080918e2b7754c3
Sylvain <sylvain.denyse@gmail.com>
2018-04-18 19:55:48 +0200

    add emiting event (client side)

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # back-redis-locator
 
-Socket handshake parameter sample : {"query": "params={\"uuid\":\"f65eebe2-aa4d-4fe5-80f4-0026e4cc47f7\", \"username\":\"dns\", \"radius\":\"2\"}"}
+### Socket handshake parameter sample :
 
-{"query": "params={\"uuid\":\"09fd65a1-bc60-4c49-8fe5-95df63082f1c\", \"username\":\"dns\", \"radius\":\"2\"}"}
+`{"query": "params={\"uuid\":\"f65eebe2-aa4d-4fe5-80f4-0026e4cc47f7\", \"username\":\"dns\", \"radius\":\"2\"}"}`
+
+### Listening events :
+
+#### addGeo :
+
+`{"uuid":"1bf92864-37e5-4622-aeda-331a10ce1fac","long":"49.99999970197677612","lat":"-10.00000092823272979"}`
+
+### Emitting events :
+
+#### setRadius :
+
+`{"radius":"3"}`
+
+#### setUuid :
+
+`{"uuid":"09fd65a1-bc60-4c49-8fe5-95df63082f1c"}`
--- src/api/geoListener.js
+++ src/api/geoListener.js
@@ -7,9 +7,26 @@ export const GeoListener = (server, io, client, schema) => {
 
     io.on('connection', socket => {
         const params = JSON.parse(socket.handshake.query.params)
-        const newKeyValue = { socket: socket, uuid: params.uuid.toString(), radius: params.radius }
+        const newNode = { socket: socket, uuid: params.uuid.toString(), radius: params.radius }
 
-        socketList.push(newKeyValue)
+        socketList.push(newNode)
+
+        socket.on('setUuid', data => {
+            const node = socketList[socketList.findIndex(el => el.uuid === params.uuid)]
+            socketList = socketList.filter(el => el.uuid != params.uuid)
+
+            node.uuid = data.uuid
+            params.uuid = data.uuid
+            socketList.push(node)
+        })
+
+        socket.on('setRadius', data => {
+            const node = socketList[socketList.findIndex(el => el.uuid === params.uuid)]
+            socketList = socketList.filter(el => el.uuid != params.uuid)
+
+            node.radius = data.radius
+            socketList.push(node)
+        })
 
         socket.on('disconnect', () => {
             const socketUuid = socketList.find(el => el.uuid === params.uuid)
@@ -20,17 +37,17 @@ export const GeoListener = (server, io, client, schema) => {
 
     client.sub.subscribe('mapsChannel')
     client.sub.on('message', async (channel, uuid) => {
-        await socketList.forEach(keyValue => {
+        await socketList.forEach(node => {
             client.pub.georadiusbymember(
                 'maps',
-                keyValue.uuid,
-                keyValue.radius,
+                node.uuid,
+                node.radius,
                 'km',
                 async (err, reply) => {
                     if (!err) {
                         if ((await reply.indexOf(uuid.toString())) > -1) {
                             client.pub.geopos('maps', uuid.toString(), async (err, reply) => {
-                                await keyValue.socket.emit('addGeo', {
+                                await node.socket.emit('addGeo', {
                                     uuid: uuid.toString(),
                                     long: reply[0][0],
                                     lat: reply[0][1]
@@ -40,7 +57,6 @@ export const GeoListener = (server, io, client, schema) => {
                     }
                 }
             )
-            console.log(keyValue.uuid)
         })
     })
 
